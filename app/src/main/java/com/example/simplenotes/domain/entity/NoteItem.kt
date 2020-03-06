662bd6cbb88a3ced4b908dc615053200b5335b82
Kirill <ueurhfyh1@gmail.com>
2020-03-06 23:29:06 +0300

    fixing bugs

--- app/src/main/java/com/example/simplenotes/domain/entity/NoteItem.kt
+++ app/src/main/java/com/example/simplenotes/domain/entity/NoteItem.kt
@@ -3,7 +3,7 @@ package com.example.simplenotes.domain.entity
 import java.io.Serializable
 
 
-class NoteItem(
+data class NoteItem(
     val title: String,
     val text: String,
     val timeCreated: String? = null,
